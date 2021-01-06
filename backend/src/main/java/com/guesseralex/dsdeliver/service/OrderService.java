52ecdd8b75bdd5cbfd9e0200e7ab981cb1c92b0c
Alex Guesser <alex.guesser@hotmail.com>
2021-01-05 22:25:28 -0300

    PUT /orders/{id}/delivered

--- backend/src/main/java/com/guesseralex/dsdeliver/service/OrderService.java
+++ backend/src/main/java/com/guesseralex/dsdeliver/service/OrderService.java
@@ -41,5 +41,14 @@ public class OrderService {
       order = repository.save(order);
       return new OrderDTO(order);
    }
+   
+   @Transactional
+   public OrderDTO setOrderStatusToDelivered(Long id) {
+      Order order = repository.getOne(id);
+      order.setStatus(OrderStatus.DELIVERED);
+      order = repository.save(order);
+      return new OrderDTO(order);
+   }
+   
 
 }
